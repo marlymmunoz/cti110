196e7583b7d7a9b14ceb142a7fec396861544eb1
marlymmunoz <munozcem0066@student.faytechcc.edu>
2025-02-06 14:20:10 -0500

    Add files via upload

---
+++ P1LAB1_MunozMarlym.py
@@ -0,0 +1,10 @@
+#Marlym Munoz
+#2-6-25
+#P1LAB1
+#This program will demo python code
+
+fname = input("Enter your first name: ")
+
+lname = input("Enter your last name: ")
+
+print("Hello, " + str(fname)+" " + str(lname)+ "! Welcome to CTI-110")
